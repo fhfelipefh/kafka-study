87c717d20aecc983f4cc18870d992854a55c4153
Felipe Honorio Moraes <felipe.honoriomoraes@thomsonreuters.com>
2022-02-20 22:00:01 -0300

    Basic consumer and producer

---
+++ src/main/java/com/fhfelipefh/kafka/ConsumerMain.java
@@ -0,0 +1,35 @@
+package com.fhfelipefh.kafka;
+
+import java.time.Duration;
+import java.util.Properties;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+public class ConsumerMain {
+
+  public static void main(String[] args) {
+    final String BOOTSTRAP_SERVERS = "127.0.0.1:29092";
+    Properties properties = new Properties();
+    properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+    properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+    properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+    properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "my-app");
+    KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
+    consumer.subscribe(java.util.Arrays.asList("first_topic"));
+    System.out.println("O consumidor está aguardando mensagens...");
+    while (true) {
+      ConsumerRecords<String, String> cont = consumer.poll(Duration.ofMillis(1000));
+      if (cont.count() > 0) {
+        for (ConsumerRecord<String, String> entry : cont) {
+          System.out.println(entry.value());
+        }
+      }
+    }
+
+  }
+}
--- src/main/java/com/fhfelipefh/kafka/Main.java
+++ src/main/java/com/fhfelipefh/kafka/Main.java
@@ -1,9 +1,34 @@
 package com.fhfelipefh.kafka;
 
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
 public class Main {
 
-  public static void main(String[] args) {
- 
+  public static void main(String[] args) throws ExecutionException, InterruptedException {
+    final String BOOTSTRAP_SERVERS = "127.0.0.1:29092";
 
+    //create producer properties
+    Properties properties = new Properties();
+    properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+    properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+                           StringSerializer.class.getName());
+    properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+                           StringSerializer.class.getName());
+    //create producer
+    KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+    //create producer record
+    ProducerRecord<String, String> record = new ProducerRecord<String, String>("first_topic", "hello word!");
+    //send data
+    try {
+      producer.send(record).get();
+      System.out.println("send data");
+    } catch (Exception e) {
+      System.out.println("Msg not sent");
+    }
   }
 }
