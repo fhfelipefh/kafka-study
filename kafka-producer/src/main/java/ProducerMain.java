e89b7c25b0bafcba9c99b9d46e67b62ba8bf9992
Felipe Honorio Moraes <felipe.honoriomoraes@thomsonreuters.com>
2022-02-24 22:06:55 -0300

    studing producer and callBack

--- kafka-producer/src/main/java/ProducerMain.java
+++ kafka-producer/src/main/java/ProducerMain.java
@@ -1,86 +1,77 @@
+import static java.lang.Integer.parseInt;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 import java.util.Scanner;
 import java.util.concurrent.ExecutionException;
+import java.util.logging.Logger;
+import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 public class ProducerMain {
 
   public static void main(String[] args) throws ExecutionException, InterruptedException {
+    Logger logger = Logger.getLogger(ProducerMain.class.getName());
     final String BOOTSTRAP_SERVERS = "127.0.0.1:29092";
     String message = "";
     List<String> messages = new ArrayList<>();
     int option = -1;
-    System.out.println("Enter the message to be sent to the topic");
     Scanner scanner = new Scanner(System.in);
-    while (option != 0) {
+    while (option < 0) {
       System.out.println("1 - Send a single message");
-      System.out.println("2 - Send multiple messages");
       System.out.println("0 - Exit");
-      option = scanner.nextInt();
+      option = parseInt(scanner.nextLine());
       switch (option) {
         case 1:
           System.out.println("Enter the message to be sent to the topic");
           try {
             message = scanner.nextLine();
             messages.add(message);
+            System.out.println("Message to be sent: " + message);
           } catch (RuntimeException e) {
             System.out.println("Exception while reading the message");
-            message = e.getMessage() + " :)";
           }
-          System.out.println("Message to be sent: " + message);
-          break;
-        case 2:
-          System.out.println("Enter the number of messages to be sent to the topic");
-          int numberOfMessages = scanner.nextInt();
-          for (int i = 0; i <= numberOfMessages; i++) {
-            try {
-              message = scanner.nextLine();
-              messages.add(message);
-              System.out.println("Message added, next message: ");
-            } catch (RuntimeException e) {
-              System.out.println("Exception while reading the message");
-              message = e.getMessage() + " :)";
-            }
-          }
-          System.out.println("All messages added");
           break;
         case 0:
           System.out.println("Exiting");
           break;
       }
     }
-    if(option != 0) {
+    if(option > 0) {
+      System.out.println("Initializing the producer");
     //create producer properties
     Properties properties = new Properties();
     properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
     properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
     properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
     //create producer
-    try (KafkaProducer<String, String> producer = new KafkaProducer<>(properties)) {
-      //create producer recordProducer
-      for (int i = 0; i < messages.size(); i++) {
-        ProducerRecord<String, String> recordProducer = new ProducerRecord<>("first_topic", messages.get(i));
-        //send data
-        try {
-          producer.send(recordProducer, (metadata, exception) -> {
-            if (exception != null) {
-              exception.printStackTrace();
-              System.out.println("Error sending data: " + metadata);
-            }
-            System.out.println("Data sent successfully: " + metadata.topic());
-          }).get();
-          System.out.println("send data");
-        } catch (Exception e) {
-          System.out.println("Msg not sent");
+      KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+    //create recorder
+      ProducerRecord<String, String> recordProducer = new ProducerRecord<>("first_topic", messages.get(0));
+
+      producer.send(recordProducer, new Callback() {
+        public void onCompletion(RecordMetadata metadata, Exception exception) {
+          if (exception == null) {
+            logger.info("Received new metadata. \n"
+                            + "Topic: " + metadata.topic()
+                            + "\n" + "Partition: " + metadata.partition()
+                            + "\n" + "offset: " + metadata.offset()
+                            + "\n" + "Timestamp: " + metadata.timestamp());
+          } else {
+            logger.info("Error " + exception.getMessage());
+          }
         }
-      }
+      });
+
+      producer.close();
       messages.clear();
-    }
     }
   }
 }
